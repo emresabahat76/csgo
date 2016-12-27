6b96d0a2ad65dd1dfa4fb390799c2dac57c444b6
Çağlar Kabaca <elusiz070smurf@gmail.com>
2016-12-27 21:15:43 +0300

    8- Hata Var Ancak Düzeltilecek

--- cs go/Counter Strike  Global Offensive/Counter Strike  Global Offensive/Form2.Designer.cs
+++ cs go/Counter Strike  Global Offensive/Counter Strike  Global Offensive/Form2.Designer.cs
@@ -28,7 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(641, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(668, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Giriş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // Form2
             // 
@@ -38,12 +68,21 @@
             this.BackgroundImage = global::Counter_Strike__Global_Offensive.Properties.Resources._587593;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(790, 491);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- cs go/Counter Strike  Global Offensive/Counter Strike  Global Offensive/Form2.cs
+++ cs go/Counter Strike  Global Offensive/Counter Strike  Global Offensive/Form2.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using HtmlAgilityPack;
 
 namespace Counter_Strike__Global_Offensive
 {
@@ -19,8 +22,48 @@ namespace Counter_Strike__Global_Offensive
 
         private void button1_Click(object sender, EventArgs e)
         {
-            comboBox1.Text = label2.Text;
-            numericUpDown1.Value = int.TryParse(lavel3.text);
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("Lütfen düzgün bişeyler giriniz..");
+            }else
+            {
+                /*
+                Uri url = new Uri("https://csgosquad.com/search/" + textBox1.Text);
+
+                WebClient client = new WebClient();
+                string html = client.DownloadString(url);
+                HtmlAgilityPack.HtmlDocument dokuman = new HtmlAgilityPack.HtmlDocument();
+                dokuman.LoadHtml(html);
+                HtmlNodeCollection basliklar = dokuman.DocumentNode.SelectNodes("</tbody>");
+
+                foreach(var baslik in basliklar)
+                {
+                    string link = baslik.Attributes["href"].Value;
+                    listBox1.Items.Add(baslik.InnerText);
+                }
+                */
+                string adres = "https://csgosquad.com/search/" + textBox1.Text; //1
+                WebRequest istek = HttpWebRequest.Create(adres); //2
+                WebResponse cevap; //3
+                cevap = istek.GetResponse(); //4
+                StreamReader donenBilgiler = new StreamReader(cevap.GetResponseStream()); //5
+                string gelen = donenBilgiler.ReadToEnd(); //6
+                int titleIndexBaslangici = gelen.IndexOf("<tbody>") + 1; //7
+                int titleIndexBitisi = gelen.Substring(titleIndexBaslangici).IndexOf("</table>");
+
+                label1.Text = gelen.Substring(gelen.IndexOf("<tbody>") + 1, gelen.Substring(titleIndexBaslangici).IndexOf("</table>"));
+
+            }
         }
     }
 }
